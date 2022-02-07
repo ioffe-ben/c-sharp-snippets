d5ef03b956bbb86411c8737ed0c2b455011cad66
Ioffe Ben <ben.ioffe@aol.com>
2022-02-07 08:54:18 -0500

    Update EmailFolderRedirect.cs

--- EmailFolderRedirect.cs
+++ EmailFolderRedirect.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Windows.Forms;
+// Add EWS nuget reference
 using Microsoft.Exchange.WebServices.Data;
 
 namespace EmailFolderRedirect
@@ -11,9 +12,11 @@ namespace EmailFolderRedirect
         public fMain()
         {
             InitializeComponent();
+            // Initiate connection to EWS
             ConnectToExchangeServer();
         }
-
+        
+        // Function for AutodiscoverUrl
         static bool RedirectionCallback(string url)
         {
             return url.ToLower().StartsWith("https://");
@@ -23,11 +26,14 @@ namespace EmailFolderRedirect
         {
             try
             {
+                // Set a new exchange instance
                 exchange = new ExchangeService();
                 exchange.Credentials = new WebCredentials("USERNAME", "PASSWORD", "DOMAIN");
-                exchange.AutodiscoverUrl("EMAIL@ADDRESS.COM", RedirectionCallback);             
+                exchange.AutodiscoverUrl("EMAIL@ADDRESS.COM", RedirectionCallback);  
+                // Set and start a 1 min timer
                 Timer timer = new Timer { Interval = 60000 };    
-                timer.Enabled = true;    
+                timer.Enabled = true;
+                // Call MainLogic function every 1 min/timer tick
                 timer.Tick += new System.EventHandler(MainLogic);  
             }
             catch (Exception ex){ }
@@ -35,20 +41,25 @@ namespace EmailFolderRedirect
        
         public static void MainLogic(object source, EventArgs e)    
         {
+            // Define 1st mailbox with "Inbox" folder
             Mailbox firstMailbox = new Mailbox("EMAIL_1@ADDRESS.COM");
             FolderId firstMailboxFolder = new FolderId(WellKnownFolderName.Inbox, firstMailbox);
             Folder BindFirstMailboxFolder = Folder.Bind(exchange, firstMailboxFolder);
 
+            // Define 2nd mailbox with "Drafts" folder
             Mailbox secondMailbox = new Mailbox("EMAIL_2@ADDRESS.COM");
-            FindItemsResults<Item> secondMailbox_findResults = exchange.FindItems(new FolderId(WellKnownFolderName.Inbox, secondMailbox), new ItemView(10));
-
+            FindItemsResults<Item> secondMailbox_findResults = exchange.FindItems(new FolderId(WellKnownFolderName.Drafts, secondMailbox), new ItemView(100));
+            
+            // Check the 2nd mailbox "Drafts" folder if there are any emails there
             if (secondMailbox_findResults.Items.Count > 0)
             {
+                // Loop through 2nd mailbox "Drafts" folder emails
                 foreach (Item item in secondMailbox_findResults.Items)
                 {
                     item.Load();
                     ItemId ItemToMoveId = new ItemId(item.Id.UniqueId.ToString());
                     Item ItemToMove = Item.Bind(exchange, ItemToMoveId);
+                    // Move selected email from the 2nd mailbox "Drafts" folder to the 1st mailbox "Inbox" folder
                     ItemToMove.Move(BindFirstMailboxFolder.Id);
                 }
             }
